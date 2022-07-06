4ee321c7c2f4831b76ca36ca55941cf67cd2e361
Mark Pavel <markpavel02@gmail.com>
2022-07-06 15:49:30 -0400

    Added Preliminary Forum Model

--- models/Subscription.js
+++ models/Subscription.js
@@ -13,16 +13,28 @@ Subscription.init(
             primaryKey: true,
             autoIncrement: true
         },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         },
+        forum_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'forum',
+                key: 'id'
+            }
+        }
     },    
     {
         sequelize,
         freezeTableName: true,
         underscored: true,
-        modelName: 'forum'
+        modelName: 'subscription'
     }
 );
 
