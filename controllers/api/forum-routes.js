60d5961b12cc389eb60825f8236bbd6b6af42060
Mark Pavel <markpavel02@gmail.com>
2022-07-07 21:03:21 -0400

    Added Forum CRUD Routes

---
+++ controllers/api/forum-routes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { Forum } = require('../../models');
+
+router.get('/', (req, res) => {
+    Forum.findAll()
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Forum.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No forum found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    Forum.create({
+        title: req.body.title
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+    Forum.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No forum found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//I made this route just in case we end up using it,
+//But it will stay commented out until actually used.
+/*router.delete('/:id', (req, res) => {
+    Forum.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No forum found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});*/
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const forumRoutes = require('./forum-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/forums', forumRoutes);
 
 module.exports = router;
