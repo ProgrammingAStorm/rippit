7954a545c28076f76f9602e56069209e8d7d9f30
Dakotah Ulrath <dulrath@live.com>
2022-07-11 14:39:00 -0400

    got the homepage to render through handlebars

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars')
+
 
 const app = express();
+const hbs = exphbs.create({})
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-  })
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+//   })
 
 app.use(routes)
 
