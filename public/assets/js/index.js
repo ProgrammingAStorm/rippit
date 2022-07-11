8dcf62b440e2541e6415f7639e97cab9f421d832
Dakotah Ulrath <dulrath@live.com>
2022-07-11 14:20:47 -0400

    got the landing page to load on start

---
+++ public/assets/js/index.js
@@ -0,0 +1,9 @@
+const LoginBtn = document.getElementById('login')
+
+LoginFunction = () => {
+   
+    
+}
+
+
+LoginBtn.addEventListener('click', LoginFunction())
--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+  })
 
 app.use(routes)
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening to: ${PORT}`));
