fba94f781b67d1cbc4264497023d793374c081bd
Dakotah Ulrath <dulrath@live.com>
2022-07-12 00:13:44 -0400

    working on single posts within forums, showing all posts in 1 forum, and showing all forums

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,7 +48,47 @@ router.get('/', (req, res) => {
     console.log(req.session)
 })
 
+router.get('/post/:id', (re, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({message: 'Could not find a post with this ID. Please check the ID and try again.'})
+            return
+        }
 
-router.get('/forums')
+        const post = dbPostData.get({plain: true})
+
+        res.render('single-post', {post})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
 
 module.exports = router;
