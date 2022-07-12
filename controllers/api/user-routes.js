8deb0c3c0292a2a3bd6bb48454aeb12742e730d0
Dakotah Ulrath <dulrath@live.com>
2022-07-11 23:54:12 -0400

    logout function/alert to let you know youve successfully logged out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -92,7 +92,13 @@ router.post("/login", (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end()
+    })
+  } else {
+    res.status(404).end()
+  }
 })
 
 router.put("/:id", (req, res) => {
